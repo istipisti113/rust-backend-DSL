ff0745198f1ffba4477f80c898494261219ae420
Szabo Istvan <pisti4395@gmail.com>
2026-04-24 23:54:51 +0200

    variables

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,70 @@
-use std::{env, fs};
+use std::{any::Any, collections::HashMap, env, fs, vec};
 
 static KEYWORDS: [&str; 1] = [
     "let",
 ];
 
-enum TokenType {
-    Keyword,
-    Identifier,
-    Operator
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum Keyword {
+    Let
 }
 
-enum OperatorType {
+trait GetText {
+    fn get_text(&self)-> Option<&String>;
+    fn into_text(self)-> Option<String>;
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum Variable {
+    String(String),
+    Integer(i32)
+}
+
+impl GetText for Variable{
+    fn get_text(&self)-> Option<&String>{
+        match self {
+            Variable::String(value) => Some(&value),
+            _ => None
+        }
+    }
+
+    fn into_text(self)-> Option<String>{
+        match self {
+            Variable::String(value) => Some(value),
+            _ => None
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+enum Token {
     Plus,
-    Minus,
-    Equal
+    EqualEqual,
+    NotEqual,
+    Equal,
+    Number(u32),
+    Keyword(Keyword),
+    Identifier(String),
+    StringLiteral(String),
+    Invalid,
 }
 
-struct Token{
-    Type: TokenType,
-    Value: Option<String>, 
-    Operator: Option<OperatorType>,
+impl GetText for Token {
+    fn get_text(&self)-> Option<&String>{
+        match self {
+            Token::Identifier(value) => Some(&value),
+            Token::StringLiteral(value) => Some(&value),
+            _ => None
+        }
+    }
+
+    fn into_text(self)-> Option<String>{
+        match self {
+            Token::Identifier(value) => Some(value),
+            Token::StringLiteral(value) => Some(value),
+            _ => None
+        }
+    }
 }
 
 fn main() {
@@ -29,7 +74,8 @@ fn main() {
     if let Some(filenev) = args.get(0){
         match fs::read_to_string(filenev) {
             Ok(code)=>{
-                tokenize(code);
+                let tokenized = tokenize(code);
+                evaluate(tokenized);
             }
             Err(e)=>{println!("{}", e)}
         };
@@ -37,15 +83,85 @@ fn main() {
 }
 
 //simple tokenization for now
-fn tokenize(code: String)-> Vec<Vec<TokenType>>{
-    for token in code.split(" "){
-        if KEYWORDS.contains(&token){
+fn tokenize(code: String)-> Vec<Vec<Token>>{
+    let mut tokenized : Vec<Vec<Token>> = vec![];
+    for line in code.split("\n"){
+        let mut char_indices = line.char_indices().peekable();
+        let mut tokens : Vec<Token> = vec![];
+        while let Some((pos, ch)) = char_indices.next() {
+            println!("--{}--", ch);
+            let token = match ch {
+                '+' => Token::Plus,
+                '=' => {
+                    match char_indices.next_if_eq(&(pos+1, '=')) {
+                        Some(_equals) => Token::EqualEqual,
+                        None => Token::Equal,
+                    }
+                },
+                '!' => {
+                    match char_indices.next_if_eq(&(pos+1, '=')) {
+                        Some(_equals) => Token::NotEqual,
+                        None => Token::Invalid,
+                    }
+                },
 
+                '\"' => {
+                    let mut last_matched: char = '\0';
+                    let s: String = char_indices
+                        .by_ref()
+                        .take_while(|(_pos, c)| { 
+                            last_matched = *c;
+                            *c != '"' 
+                        })
+                        .map(|(_pos, c)| { c })
+                        .collect();
+
+                    match last_matched {
+                        '"' => Token::StringLiteral(s),
+                        _ => Token::Invalid,
+                    }
+                },
+                character => {
+                    if character.is_ascii() && character!=' '{
+                        let s: String = char_indices
+                            .by_ref()
+                            .take_while(|(_pos, c)| { 
+                                *c != ' ' && c.is_ascii()
+                            })
+                            .map(|(_pos, c)| { c })
+                            .collect();
+                        matchkeyword(character.to_string()+&s)
+                    } else {
+                        Token::Invalid
+                    }
+                },
+            };
+            tokens.push(token);
         }
-    };
-    vec![]
+        tokenized.push(tokens.into_iter().filter(|token| *token != Token::Invalid).collect());
+    }
+    tokenized.into_iter().filter(|line|*line!=vec![]).collect()
 }
 
-fn evaluate(code: String){
+fn matchkeyword(token: String) -> Token {
+    match token.as_ref() {
+        "let" => Token::Keyword(Keyword::Let),
+        _ => Token::Identifier(token)
+    }
+}
 
+fn evaluate(code: Vec<Vec<Token>>){
+    let mut variables: HashMap<String, Variable> = HashMap::new();
+    println!("{:?}", code);
+    for line in code.into_iter(){
+        if line[0] == Token::Keyword(Keyword::Let){
+            match &line[1] {
+                Token::Identifier(ident) => {
+                    variables.insert(ident.clone(), Variable::String(line[3].clone().into_text().unwrap()));
+                }
+                _ => {}
+            }
+        }
+    }
+    dbg!(variables);
 }
